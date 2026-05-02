8b059eaa4d5a1e28b50f43151c605909e4505cee
abhigna2627 <abhigna2324@gmail.com>
2026-05-02 21:43:32 +0530

    Add: majority-element - submission-7

---
+++ Data Structures & Algorithms/majority-element/submission-7.py
@@ -0,0 +1,15 @@
+class Solution:
+    def majorityElement(self, nums):
+        freq = {}
+
+        for num in nums:
+            if num in freq: 
+                freq[num] += 1 
+            else: 
+                freq[num] = 1
+
+        for key in freq:
+            if freq[key] > len(nums) // 2:
+                return key
+
+        return nums[0]   # fallback (guarantee exists)
