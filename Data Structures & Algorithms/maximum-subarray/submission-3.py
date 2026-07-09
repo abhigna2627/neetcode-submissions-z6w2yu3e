b8efe1321802fba7238e7be534e676becc91bf7e
abhigna2627 <abhigna2324@gmail.com>
2026-07-09 07:05:25 +0530

    Add: maximum-subarray - submission-3

---
+++ Data Structures & Algorithms/maximum-subarray/submission-3.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        maxSum=nums[0]
+        currSum=nums[0]
+        n=len(nums)
+        for i in range(1,n):
+            num=nums[i]
+            currSum=max(num,currSum+num)
+            maxSum=max(maxSum,currSum)
+        return maxSum
+
+        
